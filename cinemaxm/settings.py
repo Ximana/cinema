2b7e4396f8f71cd48aed7999dad39aa8441aa670
Paulo Ximana <pauloximana@gmail.com>
2025-06-09 14:40:20 +0100

    Fix: configuracao dos arquivos estaticos

--- cinemaxm/settings.py
+++ cinemaxm/settings.py
@@ -35,7 +35,7 @@ CLOUDINARY_STORAGE = {
     'SECURE': True,
 }
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Configuração do storage padrão
 STORAGES = {
@@ -43,7 +43,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
